16b3a1f306176ee0be63c939132ff02600a83349
giulia-raineri-sdg <giulia.raineri@sdggroup.com>
2025-10-24 15:29:45 +0200

    Descrizione concisa delle modifiche fatte

---
+++ DBT_PROJECT.py
@@ -0,0 +1,14 @@
+# Inizializzazione di un progetto dbt
+#dbt init
+
+# Per cambiare directorty prima di eseguire un comando dbt
+cd dbt_project 
+
+# Verifica che che la configurazione di dbt sia corretta
+dbt debug
+
+# Esegui tutte le trasformazioni definite nel progetto dbt
+dbt run
+
+
+
